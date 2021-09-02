c68372998db1833216575fd9b9dbd558673eca4d
Mohorima Islam <51834267+mohorima@users.noreply.github.com>
2021-09-02 17:59:47 +0600

    Update RoleRequest.php

--- app/Http/Requests/RoleRequest.php
+++ app/Http/Requests/RoleRequest.php
@@ -24,7 +24,7 @@ class RoleRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|max:20',
+            'name' => ['required', Rule::unique('roles')->ignore($this->id)],
             'description' => 'max:255|nullable',
         ];
     }
