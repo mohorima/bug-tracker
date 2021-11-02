890a8989fd6d870ab4af4fd45d096f37fc99b3ab
Mohorima Islam <islammohorima@gmail.com>
2021-11-02 20:49:06 +0600

    f

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,51 +1,23 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Bugtrack') }}</title>
-
-    <!-- Icons -->
-    <link rel="icon" href="{{ asset('/images/logo-sm.svg') }}" type="image/x-icon">
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,400&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-
-        <main>
-                <div class="welcome">
-                    <img src="{{ asset('/images/logo-vertical.svg') }}" alt="">
-                    @if (Route::has('login'))
-                        @auth
-                            <a href="{{ url('/home') }}">
-                                <button type="button" class="btn btn-submit">
-                                    HOME
-                                </button>
-                            </a>
-                            
-                        @else
-                            <a href="{{ route('login') }}">
-                                <button type="button" class="btn btn-submit">
-                                    LOGIN
-                                </button>
-                            </a>
-                        @endauth
-                    @endif
-                </div>
-        </main>
+@section('content')
+    <div class="welcome">
+        <img src="{{ asset('/images/logo-vertical.svg') }}" alt="">
+        @if (Route::has('login'))
+            @auth
+                <a href="{{ url('/home') }}">
+                    <button type="button" class="btn btn-submit">
+                        HOME
+                    </button>
+                </a>
+                
+            @else
+                <a href="{{ route('login') }}">
+                    <button type="button" class="btn btn-submit">
+                        LOGIN
+                    </button>
+                </a>
+            @endauth
+        @endif
     </div>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-</body>
-</html>
+@endsection
